04c36bad3f584ab7b3e04234119a2f8d74007240
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-08-30 15:53:21 +0300

    adding doctor service

---
+++ src/app/services/doctor.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { DoctorResponse } from '../models/doctor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorService {
+  private http = inject(HttpClient);
+  private apiUrl = 'http://localhost:8080/api/doctors';
+
+  getAvailableDoctors(departmentId?: number | null, date?: string | null): Observable<DoctorResponse[]> {
+    let params = new HttpParams();
+
+    if (departmentId) {
+      params = params.set('department', departmentId.toString());
+    }
+
+    if (date) {
+      params = params.set('date', date);
+    }
+
+    return this.http.get<DoctorResponse[]>(this.apiUrl, { params });
+  }
+}
