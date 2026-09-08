96a464217841b2ebd98373e5197e3bc2477d7d75
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-08 20:27:34 +0300

    updates consultation component and service tests

--- src/app/components/consultation/consultation.spec.ts
+++ src/app/components/consultation/consultation.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Consultation } from './consultation';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('Consultation', () => {
   let component: Consultation;
@@ -9,6 +12,11 @@ describe('Consultation', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Consultation],
+      providers: [
+        provideRouter([]),           
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Consultation);
