075ee7834c7f187ef9abb04923e25bb37ee05dc3
Mostafa Kharbita <mostafakharbita@gmail.com>
2026-09-11 02:45:15 +0300

    fix: integrate authentication with appointment flow

--- src/app/services/PatientAppointmentService.ts
+++ src/app/services/PatientAppointmentService.ts
@@ -1,20 +1,28 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {PatientAppointment} from '../models/patient-appointment';
+import { PatientAppointment } from '../models/patient-appointment';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class PatientAppointmentService {
+
   private http = inject(HttpClient);
+
   private baseUrl = 'http://localhost:8080/api/patients';
 
   getUpcoming(patientId: number): Observable<PatientAppointment[]> {
-    return this.http.get<PatientAppointment[]>(`${this.baseUrl}/${patientId}/appointments/date`);
+    return this.http.get<PatientAppointment[]>(
+      `${this.baseUrl}/${patientId}/appointments/date`,
+      { withCredentials: true }
+    );
   }
 
   getPast(patientId: number): Observable<PatientAppointment[]> {
-    return this.http.get<PatientAppointment[]>(`${this.baseUrl}/${patientId}/appointments/past`);
+    return this.http.get<PatientAppointment[]>(
+      `${this.baseUrl}/${patientId}/appointments/past`,
+      { withCredentials: true }
+    );
   }
 }
