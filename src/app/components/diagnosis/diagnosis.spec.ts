df6f9199d652fc00ac04e4e545f05c5018221731
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-11 03:34:26 +0300

    resove test fail

--- src/app/components/diagnosis/diagnosis.spec.ts
+++ src/app/components/diagnosis/diagnosis.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Diagnosis } from './diagnosis';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('Diagnosis', () => {
   let component: Diagnosis;
@@ -9,6 +12,11 @@ describe('Diagnosis', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Diagnosis],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Diagnosis);
