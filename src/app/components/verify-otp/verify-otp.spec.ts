7bb9fbfd522d337a24da49ddb67bdc517020bf97
Malak Mohy <malakmohy235@gmail.com>
2026-09-11 13:51:49 +0300

    Fix VerifyOtp test

--- src/app/components/verify-otp/verify-otp.spec.ts
+++ src/app/components/verify-otp/verify-otp.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
 import { VerifyOtp } from './verify-otp';
 
@@ -9,6 +11,14 @@ describe('VerifyOtp', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [VerifyOtp],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            queryParams: of({ phone: '' })
+          }
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(VerifyOtp);
