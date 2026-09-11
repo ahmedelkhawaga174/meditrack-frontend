642ccbb926eecc84f2993ce018f0435b0f3b5c56
ahmed2026-abdelhalem <ahmedgithub2026@gmail.com>
2026-09-11 03:02:27 +0300

    add dto and service for record diadnoses feature

--- src/app/services/consultation.ts
+++ src/app/services/consultation.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { ConsultationRequest, ConsultationResponse } from '../models/consultation-response-request';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { DiagnosisResponse, RecordDiagnosisRequest } from '../models/record-diagnoses';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +14,9 @@ export class ConsultationService {
 
   recordConsultation(request: ConsultationRequest): Observable<ConsultationResponse> {
     return this.http.post<ConsultationResponse>(this.apiUrl, request);
+  }
+
+  recordDiagnosis(consultationId: number, request: RecordDiagnosisRequest): Observable<DiagnosisResponse> {
+    return this.http.post<DiagnosisResponse>(`${this.apiUrl}/${consultationId}/diagnoses`, request);
   }
 }
